57945a9a33b00b83726a91deb46f62f99493a8bf
Odali Wills <odaliwills@gmail.com>
2023-08-28 12:29:29 +0100

    Created a schema for users profile and associated it to the user model with reffers to the current user

---
+++ BackEnd/models/Profile.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+ const ProfileSchema = new mongoose.Schema({
+
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
+    },
+    location:{
+        type: String,
+        require: true,
+    },
+    hobbies: {
+        type: [String],
+        required: true,
+    },
+    occupation: {
+        type: String,
+        required: true
+    },
+    badge:{
+        type: String,
+    },
+    bio:{
+        type: String
+    },
+    date:{
+        type: Date,
+        default: Date.now
+    }
+ }); 
+
+ module.exports = Profile = mongoose.module('profile', ProfileSchema)
+
+ 
